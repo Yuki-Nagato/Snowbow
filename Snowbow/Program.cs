3ced0d3569b5bc5887f886fed5976404dee156d6
長門有希 <su_zhihua_2007@163.com>
2021-03-12 00:04:14 +0800

    完成demo

--- Snowbow/Program.cs
+++ Snowbow/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using RazorLight;
+using System;
+using System.Threading.Tasks;
 
 namespace Snowbow {
 	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+		static async Task Main(string[] args) {
+			var engine = new RazorLightEngineBuilder()
+				.UseEmbeddedResourcesProject(System.Reflection.Assembly.GetEntryAssembly())
+				.UseMemoryCachingProvider()
+				.Build();
+
+			string template = "<div>@Model.Content</div>";
+			var model = new { Content = "長門有希 <^_^>" };
+
+			string result = await engine.CompileRenderStringAsync("templateKey", template, model);
+			Console.WriteLine(result);
 		}
 	}
 }
